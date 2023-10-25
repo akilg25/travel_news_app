646e37c25686352692b2f8446ba3366b37411564
akilg25 <virdo2505@gmail.com>
2023-10-25 09:19:35 +0800

    create a HomeScreen class and make a scaffold for user profile section

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,40 @@ class TravelNewsApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: 
+      home: HomeScreen()
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                CircleAvatar(
+                  radius: 40.0,
+                  backgroundImage: AssetImage('assets/user_profile.jpg'),
+                ),
+                Text('Welcome Back, User',
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Text('Current Date: ${DateTime.now().toString()}'),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
